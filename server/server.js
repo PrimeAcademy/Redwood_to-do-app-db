8d0214d6b3216ecd5371933c8d6b84f80e795506
Mater Dev <aryeh.the.lion@gmail.com>
2024-09-27 13:56:01 -0500

    update port to use enviornment variable

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const taskRouter = require('./routes/task.router.js')
 
+// Configures our server to read enviornment variable file (.env)
+  // This will be inherited by the rest of your server.
+require('dotenv').config()
+
+console.log("Enviornment Variables:", process.env.PORT)
+
 // Setup bodyParser - urlencoded for jQuery, json for axios/react - Postman can do either
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
@@ -19,7 +25,7 @@ app.use( express.static( 'server/public' ) );
 app.use('/task', taskRouter);
 
 // Start listening
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen( PORT, () => {
   console.log( 'Listening on port', PORT );
 })
